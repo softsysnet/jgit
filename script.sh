5153be8c3cc1ff095168bb720cde8867640a3eb9
John Cote <Amobee.jc@gmail.com>
2017-10-21 09:00:15 -0700

    added commands

--- script.sh
+++ script.sh
@@ -1,3 +1,6 @@
-jachooto console  Sep  1 13:09 
-jachooto ttys000  Sep 13 13:47 
-jachooto ttys001  Sep 21 15:29 
+who
+#jachooto console  Sep  1 13:09 
+who
+#jachooto ttys000  Sep 13 13:47 
+who
+#jachooto ttys001  Sep 21 15:29 
